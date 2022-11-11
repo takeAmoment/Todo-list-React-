342ce6bf6832d62e2b1ff5cb30885cfec0a43005
Aliaksandra Piakhota <sasha@macbook-air-aliaksandra.home>
2022-11-11 13:27:56 +0100

    feat: get together all components

--- todo/src/App.tsx
+++ todo/src/App.tsx
@@ -1,8 +1,69 @@
 import React from 'react';
-import './App.css';
+import TODO_LIST from 'data/todoCollection';
+import { useState } from 'react';
+import { ITodo } from 'types/types';
+import Header from 'components/Header/Header';
+import './styles/App.css';
+import TodoPanel from 'components/TodoPanel/TodoPanel';
+import TodoList from 'components/TodoList/TodoList';
 
-function App() {
-  return <div className="App">Hello</div>;
-}
+const App = () => {
+  const [todoList, setTodoList] = useState<ITodo[]>(TODO_LIST);
+  const [todoIdForEdit, setTodoIdForEdit] = useState<number | null>(null);
+
+  const selectTodoIdForEdit = (id: number) => {
+    setTodoIdForEdit(id);
+  };
+  const addTodo = ({ name, description }: Omit<ITodo, 'id' | 'checked'>) => {
+    setTodoList([...todoList, { name, description, id: todoList.length + 1, checked: false }]);
+  };
+
+  const deleteItem = (id: number) => {
+    setTodoList(todoList.filter((item) => item.id !== id));
+  };
+
+  const checkedTodo = (id: ITodo['id']) => {
+    console.log('click');
+    setTodoList(
+      todoList.map((todo) => {
+        if (todo.id === id) {
+          return { ...todo, checked: !todo.checked };
+        }
+
+        return todo;
+      })
+    );
+  };
+
+  const changeTodo = ({ name, description }: Omit<ITodo, 'id' | 'checked'>) => {
+    setTodoList(
+      todoList.map((todo) => {
+        if (todo.id === todoIdForEdit) {
+          return { ...todo, name, description };
+        }
+
+        return todo;
+      })
+    );
+    setTodoIdForEdit(null);
+  };
+
+  return (
+    <div className="app__container">
+      <Header todoCount={todoList.length} />
+      <div className="todo__container">
+        <TodoPanel mode="add" addTodo={addTodo} />
+        <TodoList
+          todoIdForEdit={todoIdForEdit}
+          todoList={todoList}
+          deleteItem={deleteItem}
+          checkedTodo={checkedTodo}
+          selectTodoIdForEdit={selectTodoIdForEdit}
+          changeTodo={changeTodo}
+        />
+      </div>
+    </div>
+  );
+};
 
 export default App;
