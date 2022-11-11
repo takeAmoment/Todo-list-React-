2edb8a6fdaae0a1a877a3461df9319c3c1d44ecf
Aliaksandra Piakhota <sasha@macbook-air-aliaksandra.home>
2022-11-11 11:12:54 +0100

    feat: create Header component

---
+++ todo/src/components/Header.tsx
@@ -0,0 +1,15 @@
+import React, { FC } from 'react';
+import { IHeader } from 'types/types';
+import '../styles/Header.css';
+
+const Header: FC<IHeader> = ({ todoCount }) => {
+  return (
+    <div className="header__container">
+      <div className="header__contant">
+        ToDo List {todoCount} {todoCount > 1 ? 'tasks' : 'task'}
+      </div>
+    </div>
+  );
+};
+
+export default Header;
