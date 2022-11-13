e156959a93d3399c6c925236a53b7fd0f1e4ef63
Aliaksandra Piakhota <sasha@macbook-air-aliaksandra.home>
2022-11-13 13:03:33 +0100

    feat: replace Provider to index.tsx

--- todo/src/App.tsx
+++ todo/src/App.tsx
@@ -3,20 +3,16 @@ import Header from 'components/Header/Header';
 import './styles/App.css';
 import TodoPanel from 'components/TodoPanel/TodoPanel';
 import TodoList from 'components/TodoList/TodoList';
-import store from './redux/store/store';
-import { Provider } from 'react-redux';
 
 const App = () => {
   return (
-    <Provider store={store}>
-      <div className="app__container">
-        <Header />
-        <div className="todo__container">
-          <TodoPanel mode="add" />
-          <TodoList />
-        </div>
+    <div className="app__container" data-testid="app-container">
+      <Header />
+      <div className="todo__container">
+        <TodoPanel mode="add" />
+        <TodoList />
       </div>
-    </Provider>
+    </div>
   );
 };
 
